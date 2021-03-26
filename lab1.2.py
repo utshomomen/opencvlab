6b15e46c90cdf1e74dcc446f681bb23994a03634
utshomomen <mohibulmomenutsho@yahoo.com>
2021-03-27 02:13:02 +0300

    newcommit 4th try 1.2 lab

--- lab1.2.py
+++ lab1.2.py
@@ -5,6 +5,8 @@ import random
 import sys
 from os import path
 import matplotlib.pyplot as plt
+import matplotlib.colors
+import scipy
 
 
 def readFileAndResize():
@@ -30,27 +32,98 @@ def halfTone():
 
 halfTone()
 
+
+def build_is_hist(img):
+    pass
+    hei = img.shape[0]
+    wid = img.shape[1]
+    ch = img.shape[2]
+    Img = np.zeros((hei+4, wid+4, ch))
+    for i in range(ch):
+        Img[:,:,i] = np.pad(img[:,:,i], (2,2), 'edge')
+    hsv = (matplotlib.colors.rgb_to_hsv(Img))
+    hsv[:,:,0] = hsv[:,:,0] * 255
+    hsv[:,:,1] = hsv[:,:,1] * 255
+    hsv[hsv>255] = 255
+    hsv[hsv<0] = 0
+    hsv = hsv.astype(np.uint8).astype(np.float64)
+    fh = np.array([[-1.0,0.0,1.0],[-2.0,0.0,2.0],[-1.0,0.0,1.0]])
+    fv = fh.conj().T
+
+    H = hsv[:,:,0]
+    S = hsv[:,:,1]
+    I = hsv[:,:,2]
+
+
+
+    h = H[2:hei+2,2:wid+2]
+    s = S[2:hei+2,2:wid+2]
+    i = I[2:hei+2,2:wid+2].astype(np.uint8)
+
+
+
+    Rho = np.zeros((hei+4,wid+4))
+    for p in range(2,hei+2):
+        for q in range(2,wid+2):
+            tmpi = I[p-2:p+3,q-2:q+3]
+            tmps = S[p-2:p+3,q-2:q+3]
+            corre = np.corrcoef(tmpi.flatten('F'),tmps.flatten('F'))
+            Rho[p,q] = corre[0,1]
+
+    rho = np.abs(Rho[2:hei+2,2:wid+2])
+    rho[np.isnan(rho)] = 0
+    # rd = (rho*ds).astype(np.uint32)
+    Hist_I = np.zeros((256,1))
+    Hist_S = np.zeros((256,1))
+
+    # for n in range(0,255):
+        # temp = np.zeros(di.shape)
+        # temp[i==n] = di[i==n]
+        # Hist_I[n+1] = np.sum(temp.flatten('F'))
+        # # temp = np.zeros(di.shape)
+        # temp[i==n] = rd[i==n]
+        # Hist_S[n+1] = np.sum(temp.flatten('F'))
+
+    return Hist_I, Hist_S
+
+
+
+
+
 def increasingContrast():
-
-
-    # srcImage = cv2.imread("imagehh.jpg")
-
-    # # img = np.uint16(srcImage)
-    # # img = np.clip(2.0 * img, 0, 255)
-    # # img = np.uint8(img)
-    # # cv2.imshow(" contrast", img)
-    # cv2.waitKey(0)
-
-
-    srcImage = cv2.imread('imagehh.jpg')
-    cv2.imshow('test', srcImage)
+    img = cv2.imread('imagehh.jpg')
+    cv2.imshow('test', img)
     cv2.waitKey(0)
-    imghsv = cv2.cvtColor(srcImage, cv2.COLOR_BGR2HSV)
-
-    imghsv[:, :, 2] = [[max(pixel - 25, 0) if pixel < 190 else min(pixel + 25, 255) for pixel in row] for row in
-                       imghsv[:, :, 2]]
-    cv2.imshow('integralcontrast', cv2.cvtColor(imghsv, cv2.COLOR_HSV2BGR))
+    # alpha = 0.5
+    # hist_i, hist_s = build_is_hist(img)
+    # hist_c = alpha * hist_s + (1 - alpha) * hist_i
+    # hist_sum = np.sum(hist_c)
+    # hist_cum = hist_c.cumsum(axis=0)
+    #
+    # hsv = matplotlib.colors.rgb_to_hsv(img)
+    # h = hsv[:, :, 0]
+    # s = hsv[:, :, 1]
+    # i = hsv[:, :, 2].astype(np.uint8)
+    #
+    # c = hist_cum / hist_sum
+    # s_r = (c * 255)
+    # i_s = np.zeros(i.shape)
+    # for n in range(0, 255):
+    #     i_s[i == n] = s_r[n + 1] / 255.0
+    # i_s[i == 255] = 1
+    # hsi_o = np.stack((h, s, i_s), axis=2)
+    # result = matplotlib.colors.hsv_to_rgb(hsi_o)
+    #
+    # result = result * 255
+    # result[result > 255] = 255
+    # result[result < 0] = 0
+    # return result.astype(np.uint8)
+    img = np.uint16(img)
+    img = np.clip(2.0 * img, 0, 255)
+    img = np.uint8(img)
+    cv2.imshow("High contrast", img)
     cv2.waitKey(0)
+
 
 
 increasingContrast()
@@ -113,15 +186,32 @@ def distanceTransform():
 distanceTransform()
 
 
-def Bluring():
-    srcImage = cv2.imread("imagehh.jpg")
+def Bluring(dist):
 
-    gaussian_blur  = cv2.GaussianBlur(srcImage, (101,101),0)
-    cv2.imshow(" bluring" , gaussian_blur )
+    img = cv2.imread("imagehh.jpg")
+    integral = cv2.integral(img)
+    h = img.shape[0]
+    w = img.shape[1]
+    c = img.shape[2]
+    blur = np.zeros(img.shape, dtype=np.uint8)
+    for x in range(h):
+        for y in range(w):
+            for ch in range(c):
+                d = dist[x, y]
+                kernelX2 = min(int(x + 3 * d), h - 1)
+                kernelX1 = max(int(x - 3 * d), 0)
+                kernelY1 = max(int(y - 3 * d), 0)
+                kernelY2 = min(int(y + 3 * d), w - 1)
+
+                blur[x, y, ch] = (integral[kernelX2 + 1, kernelY2 + 1, ch] + integral[kernelX1, kernelY1, ch] -
+                                     integral[kernelX1, kernelY2 + 1, ch] - integral[kernelX2 + 1, kernelY1, ch]) // ((kernelX2 - kernelX1 + 1) * (kernelY2 - kernelY1 + 1))
+
+    cv2.imshow("Average blur" , blur)
     cv2.waitKey(0)
-    return gaussian_blur
+    return blur
 
-Bluring()
+
+Bluring(distanceTransform())
 
 
 cv2.destroyAllWindows()
