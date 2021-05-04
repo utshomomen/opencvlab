db43e9a7aaa1b27003b64eb9940043dfe984594c
utshomomen <mohibulmomenutsho@yahoo.com>
2021-05-04 19:25:00 +0300

    newcommit 5th try 2nd lab convultion fixing

--- lab2.1.py
+++ lab2.1.py
@@ -1,14 +1,21 @@
 import cv2
+import numpy
 import numpy as np
 import math
 import sys
 import random
 from os import path
 import matplotlib.pyplot as plt
+import scipy.ndimage
+import numpy as np
+
+from scipy.ndimage.filters import convolve
+
+
 
 def loadingImageFilter():
     srcImage = cv2.imread("imagehh.jpg")
-   # kernel = np.ones((5, 5), np.uint8)
+    # kernel = np.ones((5, 5), np.uint8)
 
 
 
@@ -20,90 +27,72 @@ def loadingImageFilter():
 
 loadingImageFilter()
 
-def convolution():
-    srcImage = cv2.imread("imagehh.jpg")
+
+def convolve(image, kernel, filter=0, strides=1):
+    # Cross Correlation
+    kernel = np.flipud(np.fliplr(kernel))
+
+    # Gather Shapes of Kernel + Image + Padding
+    xKernShape = kernel.shape[0]
+    yKernShape = kernel.shape[1]
+    xImgShape = image.shape[0]
+    yImgShape = image.shape[1]
+
+    # Shape of Output Convolution
+    xOutput = int(((xImgShape - xKernShape + 2 * filter) / strides) + 1)
+    yOutput = int(((yImgShape - yKernShape + 2 * filter) / strides) + 1)
+    output = np.zeros((xOutput, yOutput))
+
+    # Apply Equal Padding to All Sides
+    if filter != 0:
+        imagePadded = np.zeros((image.shape[0] + filter*2, image.shape[1] + filter*2))
+        imagePadded[int(filter):int(-1 * filter), int(filter):int(-1 * filter)] = image
+        print(imagePadded)
+    else:
+        imagePadded = image
+
+    # Iterate through image
+    for y in range(image.shape[1]):
+        # Exit Convolution
+        if y > image.shape[1] - yKernShape:
+            break
+        # Only Convolve if y has gone down by the specified Strides
+        if y % strides == 0:
+            for x in range(image.shape[0]):
+                # Go to next row once kernel is out of bounds
+                if x > image.shape[0] - xKernShape:
+                    break
+                try:
+                    # Only Convolve if x has moved by the specified Strides
+                    if x % strides == 0:
+                        output[x, y] = (kernel * imagePadded[x: x + xKernShape, y: y + yKernShape]).sum()
+                except:
+                    break
+
+    return output
+
+def processImage(image):
+  image = cv2.imread(image)
+  image = cv2.cvtColor(src=image, code=cv2.COLOR_BGR2GRAY)
+  return image
 
 
-    # edge detection filter
-    kernel = np.array([[1, 1, 1, 1, 1],
-                       [1, 1, 1, 1, 1],
-                       [1, 1, 1, 1, 1],
-                       [1, 1, 1, 1, 1],
-                       [1, 1, 1, 1, 1]])
-    kernel = kernel / sum(kernel)
+if __name__ == '__main__':
+    # Grayscale Image
+    image = processImage('imagehh.jpg')
 
-    img_rst = cv2.filter2D(srcImage, -1, kernel)
+    kernel = np.array([[1, 0, -1], [0, 0, 0], [-1, 0, 1]])
+    output = convolve(image, kernel, filter=3)
+    cv2.imshow('edge1.jpg', output)
 
-    cv2.imshow("edgedetecton", img_rst)
-    cv2.waitKey(0)
-    srcImage = grayImg = cv2.cvtColor(srcImage, cv2.COLOR_BGR2GRAY)
+    kernel = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])
+    output = convolve(image, kernel, filter=3)
+    cv2.imshow('edge2.jpg', output)
 
+    kernel = (1/16)*np.array([[1, 2, 1], [2, 4, 2], [1, 2, 1]])
+    output = convolve(image, kernel, filter=3)
+    cv2.imshow('blur.jpg', output)
 
-
-
-    #highpasfilter
-    kernel = np.array([[0.0, -1.0, 0.0],
-                       [-1.0, 4.0, -1.0],
-                       [0.0, -1.0, 0.0]])
-
-    kernel = kernel / (np.sum(kernel) if np.sum(kernel) != 0 else 1)
-
-
-
-    img_rst2 = cv2.filter2D(srcImage, -1, kernel)
-
-    cv2.imshow("highpassfilter", img_rst2)
-    cv2.waitKey(0)
-
-    #  lowpassfilter
-
-
-    kernel = np.array([[0.0, -1.0, 0.0],
-                       [-1.0, 4.0, -1.0],
-                       [0.0, -1.0, 0.0]])
-
-    kernel = kernel / (np.sum(kernel) if np.sum(kernel) != 0 else 1)
-
-
-
-    img_rst2 = cv2.filter2D(srcImage, -1, kernel)
-
-    cv2.imshow("lowpasfilter", img_rst2)
-    cv2.waitKey(0)
-
-
-     #sharpened
-    kernel = np.array([[0, -1, 0],
-                       [-1, 5, -1],
-                       [0, -1, 0]])
-
-    kernel = kernel / (np.sum(kernel) if np.sum(kernel) != 0 else 1)
-
-    img_rst2 = cv2.filter2D(srcImage, -1, kernel)
-
-    cv2.imshow("sharpened", img_rst2)
-    cv2.waitKey(0)
-
-
-    #blur
-    kernel = np.array([[1, 2, 1],
-                       [2, 4, 2],
-                       [1, 2, 1]])
-
-    kernel = kernel / (np.sum(kernel) if np.sum(kernel) != 0 else 1)
-
-    img_rst2 = cv2.filter2D(srcImage, -1, kernel)
-
-    cv2.imshow("blur", img_rst2)
-    cv2.waitKey(0)
-
-
-
-
-
-
-
-convolution()
 
 def relu(x):
     return max(0.0, x)
@@ -164,19 +153,3 @@ def ReluAndMaxPool(x):
 
 ReluAndMaxPool(relu(x=1))
 
-# def channels ():
-#     srcImage = cv2.imread("imagehh.jpg")
-#     Mat  srcImage(5, 5, CV_64FC3);
-#     Mat ch1, ch2, ch3;
-#
-#     vector < Mat > channels(3);
-#
-#     split(img, channels);
-#
-#     ch1 = channels[0];
-#     ch2 = channels[1];
-#     ch3 = channels[2];
-#
-#
-#
-#     channels()
